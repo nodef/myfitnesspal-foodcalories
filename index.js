081ae302c157f3f4a7eae6ecdf688227bc3e89d2
Subhajit Sahu <wolfram77@gmail.com>
2017-10-27 21:03:53 +0530

    :telephone_receiver: get key and name:value

--- index.js
+++ index.js
@@ -10,6 +10,20 @@ const request = function(path) {
 
 const $ = function(id) {
   return request(`/food/calories/${id}`).then((dom) => {
+    const a = {}, b = {}, document = dom.window.document;
+    const viewName = document.querySelector('.food-description');
+    if(viewName==null) return {};
+    const key = viewName.textContent.trim();
+    const servings = document.getElementById('food_entry_weight_id');
+    for(var tr of document.querySelectorAll('#nutrition-facts tr')) {
+      var tds = tr.getElementsByTagName('td');
+      var name = tds[0].textContent.trim();
+      if(name) b[name] = tds[1].textContent.trim();
+      name = tds[2].textContent.trim();
+      if(name) b[name] = tds[3].textContent.trim();
+    }
+    a[key] = b;
+    return a;
   }, (err) => {
     const dom = new jsdom.JSDOM(err.error.toString());
     throw new Error(''+err.statusCode+' - '+dom.window.document.title);
