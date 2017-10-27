43b3af9bdb151fbf8b1ecbe2a5dbef3a438bbed5
Subhajit Sahu <wolfram77@gmail.com>
2017-10-27 20:32:59 +0530

    :telephone_receiver: new request

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 'use strict';
 const http = require('http');
 const jsdom = require('jsdom');
+
+const request = function(path) {
+    // 1. make request with user-agent
+    return jsdom.JSDOM.fromURL(`http://www.myfitnesspal.com${path}`, {
+        'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+    });
+};
