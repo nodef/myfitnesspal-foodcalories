3c6ec9c85c860ba0a8b6965099fef5e6c04c1fa9
Subhajit Sahu <wolfram77@gmail.com>
2017-10-27 20:40:27 +0530

    :telephone_receiver: use 2 spaces

--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 const jsdom = require('jsdom');
 
 const request = function(path) {
-    // 1. make request with user-agent
-    return jsdom.JSDOM.fromURL(`http://www.myfitnesspal.com${path}`, {
-        'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-    });
+  // 1. make request with user-agent
+  return jsdom.JSDOM.fromURL(`http://www.myfitnesspal.com${path}`, {
+    'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
+  });
 };
 
 const $ = function(id) {
-    return request(`/food/calories/${id}`).then((dom) => {
-    }, (err) => {
-        const dom = new jsdom.JSDOM(err.error.toString());
-        throw new Error(''+err.statusCode+' - '+dom.window.document.title);
-    });
+  return request(`/food/calories/${id}`).then((dom) => {
+  }, (err) => {
+    const dom = new jsdom.JSDOM(err.error.toString());
+    throw new Error(''+err.statusCode+' - '+dom.window.document.title);
+  });
 };
 module.exports = $;
