11bc31ca6206767d62e58c0a5f54a6c86ae153cf
Subhajit Sahu <wolfram77@gmail.com>
2017-10-27 20:38:26 +0530

    :telephone_receiver: start writing module

--- index.js
+++ index.js
@@ -7,3 +7,12 @@ const request = function(path) {
         'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
     });
 };
+
+const $ = function(id) {
+    return request(`/food/calories/${id}`).then((dom) => {
+    }, (err) => {
+        const dom = new jsdom.JSDOM(err.error.toString());
+        throw new Error(''+err.statusCode+' - '+dom.window.document.title);
+    });
+};
+module.exports = $;
