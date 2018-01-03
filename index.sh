c0e357bb1594ae5d6b7b0239146e327b9c59dbf3
Subhajit Sahu <wolfram77@gmail.com>
2018-01-03 15:21:37 +0530

    :snowman: update start script (linux)

--- index.sh
+++ index.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-dp0=$(dirname "$0")/
+psd() {
+  # present script directory
+  z="${BASH_SOURCE[0]}"
+  if [ -h "$z" ]; then z="$(readlink "$z")"; fi
+  cd "$(dirname "$0")" && cd "$(dirname "$z")" && pwd
+}
+
+# start main
+dp0="$(psd)/"
 node "$dp0." "$@"
