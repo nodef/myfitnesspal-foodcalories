6b8da7916f0f665430e6f9b57a137cb21a5dadb1
Subhajit Sahu <wolfram77@gmail.com>
2018-01-03 15:44:22 +0530

    :snowman: split into parts

--- index.js
+++ index.js
@@ -2,46 +2,43 @@
 const cheerio = require('cheerio');
 const scrapeArange = require('terminal-scrapearange');
 
-const request = function(path) {
-  // 1. make request with user-agent
-  return jsdom.JSDOM.fromURL(`http://www.myfitnesspal.com${path}`, {
-    'userAgent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'
-  });
+
+function request(path) {
+  // 1. make request to myfitnesspal
+  var opt = {method: 'GET', hostname: 'www.myfitnesspal.com', path};
+  return scrapeArange.request(opt);
 };
 
-const $ = function(id) {
+function nameParts(z, str, id) {
+  // 1. get details from name
+  z['Number'] = id;
+  z['Name'] = str.trim();
+};
+
+function headParts(z, $, elm) {
+  // 1. get details from head
+  z['Servings'] = elm.find('option:selected').text().trim();
+};
+
+function bodyParts(z, $, elm) {
+  // 1. get details from body
+  var tds = $(elm).find('td');
+  var name = $(tds[0]).text().trim();
+  if(!!name) z[name] = $(tds[1]).text().trim();
+  name = $(tds[2]).text().trim();
+  if(!!name) z[name] = $(tds[3]).text().trim();
+};
+
+function searchAll(id) {
   return request(`/food/calories/${id}`).then((dom) => {
-    const a = {}, b = {}, document = dom.window.document;
-    const viewName = document.querySelector('.food-description');
-    if(viewName==null) return {};
-    b['Name'] = viewName.textContent.trim();
-    const servings = document.getElementById('food_entry_weight_id');
-    b['Servings'] = servings.selectedOptions[0].textContent;
-    for(var tr of document.querySelectorAll('#nutrition-facts tr')) {
-      var tds = tr.getElementsByTagName('td');
-      var name = tds[0].textContent.trim();
-      if(name) b[name] = tds[1].textContent.trim();
-      name = tds[2].textContent.trim();
-      if(name) b[name] = tds[3].textContent.trim();
-    }
-    a[id] = b;
-    return a;
-  }, (err) => {
-    const dom = new jsdom.JSDOM(err.error.toString());
-    throw new Error(''+err.statusCode+' - '+dom.window.document.title);
+    var $ = cheerio.load(html), z = {'Id': id};
+    var foodDesc = $('.food-description');
+    if(!foodDesc.length) throw new Error('Bad request id');
+    nameParts(z, foodDesc.text(), id);
+    headParts(z, $, $('#food_entry_weight_id'));
+    $('#nutrition-facts tr').each((i, elm) => bodyParts(z, $, elm));
+    return z;
   });
 };
 module.exports = $;
-
-if(require.main===module) {
-  const z = {}, a = process.argv;
-  const start = parseInt(a[2], 10)||0, stop = parseInt(a[3], 10)||start+1;
-  const step = parseInt(a[4], 10)||1, inc = Math.sign(step);
-  const fetch = (id) => pro.then(() => $(id)).then((ans) => Object.assign(z, ans));
-  for(var i=start, pro=Promise.resolve(); i!==stop;) {
-    for(var I=Math.min(stop, i+step), p=[]; i!==I; i+=inc)
-      p.push(fetch(i));
-    pro = Promise.all(p);
-  }
-  pro.then(() => console.log(JSON.stringify(z)));
-}
+if(require.main===module) scrapeArange.main({method: searchAll});
